0175ed0516e5535081bf35e04cba888589ae2316
RdkSki <rotax@gmx.net>
2021-09-22 14:37:45 +0200

    advanced finished

--- scriptad1.js
+++ scriptad1.js
@@ -4,5 +4,20 @@ var c = "21";
 var d = '36';
 var e = 42;
 
-var f= a + b + Number(c) + Number(d) + e;
-console.log(f);
+var x= a + b + Number(c) + Number(d) + e;
+console.log(x);
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var y= Number(f) * g * h * Number(i);
+console.log(y);
+
+var z=x/y;
+console.log(z);
+document.write(z);
