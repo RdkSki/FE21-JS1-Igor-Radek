80d10f9ff2615da3b13d50a2a459d3096deaa6c9
RdkSki <rotax@gmx.net>
2021-09-22 15:18:56 +0200

    ch1 displayed on screen

--- scriptch1.js
+++ scriptch1.js
@@ -5,3 +5,8 @@ var a =[["Vienna", " ", " ", " ", " "],
        [" "," "," "," ","city"]];
 
 console.table(a);
+document.write(a[0][0]);
+document.write(a[1][1]);
+document.write(a[2][2]);
+document.write(a[3][3]);
+document.write(a[4][4]);
