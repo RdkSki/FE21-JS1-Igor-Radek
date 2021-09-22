5c65c540b76717e1837c086eebe17cdbe072359e
RdkSki <rotax@gmx.net>
2021-09-22 15:30:50 +0200

    fixed extra2

--- scriptex2.js
+++ scriptex2.js
@@ -1,4 +1,4 @@
-var animals= ['apple', 'banana', 'kiwi'];
-console.log(animals);
-animals.splice(2,0,"orange","strawberry");
-console.log(animals);
+var fruits= ['apple', 'banana', 'kiwi'];
+console.log(fruits);
+fruits.splice(2,0,"orange","strawberry");
+console.log(fruits);
